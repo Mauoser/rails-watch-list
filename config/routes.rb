832000fdb51c1f7c1420bd70897a16051ecee759
Rei Zhang <mauoser@outlook.com>
2024-07-11 21:26:24 +0900

    Updated homepage

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,12 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "lists#index"
+
+  resources :lists, only: [:index, :new, :create, :show] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
+  post "lists", to: "lists#create"
+  post "bookmarks", to: "bookmarks#create"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+movies_serialized = URI.open(url).read
+top_movies = JSON.parse(movies_serialized)
+base_url = "https://image.tmdb.org/t/p/w500"
+results = top_movies["results"]
+
+results.take(10).each do |result|
+  Movie.create(title: result["title"], overview: result["overview"], poster_url: base_url + result["poster_path"], rating: result["vote_average"])
+end
